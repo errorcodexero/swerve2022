83da62e8de235dd82a2ad416bcd232cb5843675a
Cowington Post <cowingtonpost@gmail.com>
2022-10-21 11:41:41 -0700

    add getProperty for simulation

--- src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.climber;
 
 import org.xero1425.base.subsystems.Subsystem;
 import org.xero1425.base.subsystems.motorsubsystem.MotorEncoderSubsystem;
+import org.xero1425.misc.SettingsValue;
 
 public class ClimberSubsystem extends MotorEncoderSubsystem {
 
@@ -9,4 +10,14 @@ public class ClimberSubsystem extends MotorEncoderSubsystem {
         super(parent, "climber", false);
     }
 
+    @Override
+    public SettingsValue getProperty(String name) {
+        SettingsValue v = null ;
+
+        if (name.equals("position")) {
+            v = new SettingsValue(getPosition()) ;
+        }
+
+        return v ;
+    }
 }
